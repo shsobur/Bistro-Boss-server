3b1b2bad68a92bfdaadf0e81af7f3c891743ded5
shsobur <shsoburhossen951@gmail.com>
2024-07-01 17:11:07 +0600

    mongodb added

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ async function run() {
     const menuCollection = client.db("bistroDB").collection("menus");
     const reviewCollection = client.db("bistroDB").collection("reviews");
 
+    app.get("/menu", async(req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
